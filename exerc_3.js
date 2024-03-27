e513da2994f44dc8abc0a93702de2c4cbbaf6c52
JairRodriguesJr <151882315+JairRodriguesJr@users.noreply.github.com>
2024-03-27 10:34:12 -0300

    atividade

--- exerc_3.js
+++ exerc_3.js
@@ -3,3 +3,12 @@
 let h = document.getElementById('x');
 // mudar a cor
 h.style.color= 'red'
+
+//tagname
+const element = document.getElementsByTagName("h2");
+
+document.getElementById("2").innerHTML = 'Esse texto (index 0) é: ' + element[0].innerHTML;
+
+//className
+const c = document.getElementsByClassName("exemplo_cor");
+c[0].style.backgroundColor = "red";
