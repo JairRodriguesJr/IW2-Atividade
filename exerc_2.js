8995af193f6abf446684ed6160c9783d445f3075
JairRodriguesJr <151882315+JairRodriguesJr@users.noreply.github.com>
2024-03-27 09:32:20 -0300

    iw2_atividade

---
+++ exerc_2.js
@@ -0,0 +1,13 @@
+window.onload = function(){ 
+
+
+
+		let bt = document.getElementsByClassName("ClassBt");
+		console.log([0].value); 
+
+		// disparar o clique
+		bt[0].onclick = function(){
+			alert('É um número Primo');
+			
+		}
+}
---
+++ exerc_3.js
@@ -0,0 +1,5 @@
+//id
+
+let h = document.getElementById('x');
+// mudar a cor
+h.style.color= 'red'
